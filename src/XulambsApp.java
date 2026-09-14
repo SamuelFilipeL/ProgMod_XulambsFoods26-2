6074f71d247cd1ed47f5b8f5091f3dd406df0cc7
Samuel Filipe <samuelfilipelfer@gmail.com>
2026-09-14 12:18:23 -0300

    fazendo App

--- src/XulambsApp.java
+++ src/XulambsApp.java
@@ -23,15 +23,18 @@ public class XulambsApp {
 
     private int exibirMenu() {
         cabecalho();
-        IO.println("1 - Comprar pizza");
-        IO.println("2 - Ver todas as pizzas");
+        IO.println("1 - Abrir pedido");
+        IO.println("2 - Alterar pedidos");
+        IO.println("3 - Relatorio de pedido");
+        IO.println("4 - Encerrar pedido");
         IO.println("0 - Sair");
+        
         return Integer.parseInt(IO.readln("Digite sua opção: "));
     }
 
 
 
-    void comprarPizza(){
+    Pizza comprarPizza(){
         cabecalho();
         int adicionais = 
             Integer.parseInt(IO.readln("Quantos ingredientes? "));
@@ -41,6 +44,7 @@ public class XulambsApp {
 
         mostrarNota(nova);
         listaPizzas.add(nova);
+        return nova;
     }
 
     void mostrarNota(Pizza pizza){
@@ -56,14 +60,35 @@ public class XulambsApp {
         }
     }
 
+
+    void abrirPedido(){
+        String querMais = "n";
+        Pedido novoPedido = new Pedido();
+        do{
+            Pizza novaPizza = comprarPizza();
+            novoPedido.adicionarPizza(novaPizza);
+            querMais=IO.readln("Quer mais pizzas?");
+        }while(querMais.equals("s"));
+    }
+
+    void alterarPedido(){
+        cabecalho();
+        int idPedido = Integer.parseInt(IO.readln("Nº do pedido"));
+        String busca = String.format("Pedido Nº %d", idPedido);
+        for(Pedido ped : listaPedidos){
+            if(ped.relatorio().contains(busca)){
+                //achei
+            }
+        }
+    }
     void main(){
         int opcao;
         listaPizzas = new LinkedList<>();
         do {
             opcao = exibirMenu();
             switch (opcao) {
-                case 1 -> comprarPizza();
-                case 2 -> mostrarPizzas();
+                case 1 -> abrirPedido();
+                case 2 -> alterarPedido();
                 case 0 -> IO.println("Encerrando!");
                 default -> IO.println("Opção inválida");
             }   
